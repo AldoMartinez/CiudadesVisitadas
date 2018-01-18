4cdd0889e18a05d145754cc432859dd7f67d05d5
Aldo Aram Martinez Mireles <aldo.mireles.97@gmail.com>
2018-01-17 21:56:43 -0600

    Cambie el color de la ciudad Washington

--- VisitedCity's/ViewController.swift
+++ VisitedCity's/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
         city = City(name: "Los Angeles", foundation: "4 de septiembre del 1781", poblation: 3976000, country: "Estados Unidos", image: #imageLiteral(resourceName: "Los Angeles"), color: #colorLiteral(red: 1, green: 0.4544541838, blue: 0.4526622243, alpha: 1))
         cities.append(city)
         
-        city = City(name: "Washington D.C.", foundation: "16 de julio de 1790", poblation: 658893, country: "Estados Unidos", image: #imageLiteral(resourceName: "Washington"), color: #colorLiteral(red: 0.4329445382, green: 0.7821732533, blue: 1, alpha: 1))
+        city = City(name: "Washington D.C.", foundation: "16 de julio de 1790", poblation: 658893, country: "Estados Unidos", image: #imageLiteral(resourceName: "Washington"), color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
         cities.append(city)
         
         city = City(name: "New York", foundation: "1624", poblation: 8538000, country: "Estados Unidos", image: #imageLiteral(resourceName: "New York"), color: #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1))
